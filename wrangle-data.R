8373e71b71509b27b62c7c16cb27448f4ee1a97c
Vinamra Agrawal <vinamra.agrawalv@gmail.com>
2020-06-30 11:00:33 -0400

    Added new population data set and wrangle function for that data set. The function is long so I put it into its own file.

--- wrangle-data.R
+++ wrangle-data.R
@@ -5,7 +5,7 @@
 # the mutate() function and other functions that are helpful for cleaning
 # the data.
 library(tidyverse)
-covid <- read.csv("data/covid.csv", stringsAsFactors=F)
+covid <- read.csv("data/covid.csv", stringsAsFactors = FALSE)
 
 # Deleting calculated row at the end of the data (totals)
 covid <- covid[-nrow(covid),]
@@ -20,3 +20,8 @@ covid$Death.Count <- as.numeric(covid$Death.Count)
 covid$Hospitalized.Count <- as.numeric(covid$Hospitalized.Count)
 
 save(covid, file = "rdata/covid.rda")
+
+# Run population_data_wrangling_function.R file before this code to get the 
+# wrangle function for the population data.
+population <- read.csv("data/population.csv", stringsAsFactors = FALSE)
+population <- wrangle_pop(population)
